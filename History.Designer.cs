8896060c81ed6ce9e2a6d35ec89230a1704d9d89
yenlele21 <lebaoyen8a6@gmail.com>
2025-03-20 10:57:19 +0700

    suaHinh

--- History.Designer.cs
+++ History.Designer.cs
@@ -42,31 +42,35 @@
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(31, 27);
+            this.listBox1.ItemHeight = 42;
+            this.listBox1.Location = new System.Drawing.Point(46, 42);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(325, 381);
+            this.listBox1.Size = new System.Drawing.Size(486, 592);
             this.listBox1.TabIndex = 0;
             // 
             // groupBox1
             // 
-            this.groupBox1.BackgroundImage = global::Dic_AppTest.Properties.Resources._8a0f7ce44c8063f524c215a055d93544;
+            this.groupBox1.BackgroundImage = global::Dic_AppTest.Properties.Resources.histo_bgr;
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.groupBox1.Controls.Add(this.btXoaall);
             this.groupBox1.Controls.Add(this.btXoa1tu);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(409, 55);
+            this.groupBox1.Location = new System.Drawing.Point(614, 86);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(336, 97);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(504, 152);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Xóa lịch sử";
             // 
             // btXoaall
             // 
-            this.btXoaall.Location = new System.Drawing.Point(209, 46);
+            this.btXoaall.Location = new System.Drawing.Point(314, 72);
+            this.btXoaall.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btXoaall.Name = "btXoaall";
-            this.btXoaall.Size = new System.Drawing.Size(96, 36);
+            this.btXoaall.Size = new System.Drawing.Size(144, 56);
             this.btXoaall.TabIndex = 1;
             this.btXoaall.Text = "All";
             this.btXoaall.UseVisualStyleBackColor = true;
@@ -74,9 +78,10 @@
             // 
             // btXoa1tu
             // 
-            this.btXoa1tu.Location = new System.Drawing.Point(25, 46);
+            this.btXoa1tu.Location = new System.Drawing.Point(38, 72);
+            this.btXoa1tu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btXoa1tu.Name = "btXoa1tu";
-            this.btXoa1tu.Size = new System.Drawing.Size(178, 36);
+            this.btXoa1tu.Size = new System.Drawing.Size(267, 56);
             this.btXoa1tu.TabIndex = 0;
             this.btXoa1tu.Text = "Chọn để xóa";
             this.btXoa1tu.UseVisualStyleBackColor = true;
@@ -87,30 +92,31 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(410, 173);
+            this.label1.Location = new System.Drawing.Point(615, 270);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(329, 235);
+            this.label1.Size = new System.Drawing.Size(494, 367);
             this.label1.TabIndex = 2;
             this.label1.Text = "Search History";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // History
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackgroundImage = global::Dic_AppTest.Properties.Resources._03_PPT_Background_Wallpaper_Google_Slides_Background_354x234;
+            this.BackgroundImage = global::Dic_AppTest.Properties.Resources.his_bgr;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "History";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "History";
-            
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
