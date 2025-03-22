2901988c5215a520974636e472b3d821dea82c63
NguyenNhu-gif <nhu.nt2508@gmail.com>
2025-03-22 17:14:58 +0700

    updelete

---
+++ Delete_Word.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Dic_AppTest
+{
+    partial class Delete_Word
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new Dic_AppTest.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.btChange = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btNhaplai = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 99);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Delete";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.CornerRadius = 20;
+            this.panel1.Location = new System.Drawing.Point(33, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 255);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nhập từ:";
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.BackColor = System.Drawing.Color.MistyRose;
+            this.txtNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhap.Location = new System.Drawing.Point(201, 173);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(390, 30);
+            this.txtNhap.TabIndex = 3;
+            // 
+            // btChange
+            // 
+            this.btChange.BackColor = System.Drawing.Color.MistyRose;
+            this.btChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btChange.Location = new System.Drawing.Point(259, 252);
+            this.btChange.Name = "btChange";
+            this.btChange.Size = new System.Drawing.Size(168, 61);
+            this.btChange.TabIndex = 4;
+            this.btChange.Text = "Anh-Việt";
+            this.btChange.UseVisualStyleBackColor = false;
+            this.btChange.Click += new System.EventHandler(this.btChange_Click);
+            // 
+            // btLuu
+            // 
+            this.btLuu.BackColor = System.Drawing.Color.White;
+            this.btLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLuu.Location = new System.Drawing.Point(648, 157);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(122, 61);
+            this.btLuu.TabIndex = 4;
+            this.btLuu.Text = "Xóa";
+            this.btLuu.UseVisualStyleBackColor = false;
+            this.btLuu.Click += new System.EventHandler(this.btLuu_Click);
+            // 
+            // btNhaplai
+            // 
+            this.btNhaplai.BackColor = System.Drawing.Color.White;
+            this.btNhaplai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNhaplai.Location = new System.Drawing.Point(648, 233);
+            this.btNhaplai.Name = "btNhaplai";
+            this.btNhaplai.Size = new System.Drawing.Size(122, 61);
+            this.btNhaplai.TabIndex = 4;
+            this.btNhaplai.Text = "Nhập lại";
+            this.btNhaplai.UseVisualStyleBackColor = false;
+            this.btNhaplai.Click += new System.EventHandler(this.btNhaplai_Click);
+            // 
+            // btThoat
+            // 
+            this.btThoat.BackColor = System.Drawing.Color.White;
+            this.btThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThoat.Location = new System.Drawing.Point(648, 310);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(122, 61);
+            this.btThoat.TabIndex = 4;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = false;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // Delete_Word
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btNhaplai);
+            this.Controls.Add(this.btLuu);
+            this.Controls.Add(this.btChange);
+            this.Controls.Add(this.txtNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Delete_Word";
+            this.Text = "Delete_Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.Button btChange;
+        private System.Windows.Forms.Button btLuu;
+        private System.Windows.Forms.Button btNhaplai;
+        private System.Windows.Forms.Button btThoat;
+    }
+}
